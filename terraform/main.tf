fc69c465de2b8529e738dfac11ea8ba2b67ec891
BRYNSFRIGATE\bryna <brynaytes@gmail.com>
2023-08-13 17:48:20 -0500

    Adding modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,82 +1,18 @@
-locals {
-  s3_bucket_name = "${var.site_name}-site-assets"
+module "s3" {
+    source = "./modules/s3"
+    site_name = var.site_name
 }
 
-resource "aws_s3_bucket" "site_bucket" {
-    bucket =  "${var.site_name}-site-assets"
-    tags = {
-        Name = "Terraform hosting test"
-    }
+module "cf_distribution" {
+    source = "./modules/distribution"
+    site_name = var.site_name
+    site_bucket_id = module.s3.s3_id
+    site_bucket_domain_name = module.s3.s3_domain_name
 }
 
-resource "aws_cloudfront_distribution" "distribution"  {
-    origin {
-        domain_name = aws_s3_bucket.site_bucket.bucket_regional_domain_name
-        origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
-        origin_id = aws_s3_bucket.site_bucket.id
-    }
-    default_cache_behavior {
-        allowed_methods = ["POST", "HEAD", "PATCH", "DELETE", "PUT", "GET", "OPTIONS"]
-        cached_methods = [ "HEAD", "GET", "OPTIONS"]
-        viewer_protocol_policy = "allow-all"
-        target_origin_id = aws_s3_bucket.site_bucket.id
-        forwarded_values {
-          query_string = false
-              cookies {
-            forward = "none"
-          }
-        }
-
-    }
-    restrictions {
-        geo_restriction {
-
-            restriction_type = "none"
-            locations = []
-        }
-    }
-    viewer_certificate {
-        cloudfront_default_certificate = true
-    }
-    enabled = true
-    is_ipv6_enabled = true
-    default_root_object = "index.html" 
-    tags = {
-        Name = "Terraform hosting test"
-    }
+module "s3_policy" {
+  source = "./modules/s3_policy"
+    s3_bucket_name = "${var.site_name}-site-assets"
+    s3_bucket_id = module.s3.s3_id
+    cloudfront_arn = module.cf_distribution.distribution_arn
 }
-
-resource "aws_cloudfront_origin_access_control" "oac" {
-  name = "OAC for ${var.site_name}"
-  description = ""
-  origin_access_control_origin_type = "s3"
-  signing_behavior = "always"
-  signing_protocol = "sigv4"
-}
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-    bucket = aws_s3_bucket.site_bucket.id
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Id": "PolicyForCloudFrontPrivateContent",
-    "Statement": [
-        {
-            "Sid": "AllowCloudFrontServicePrincipal",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "cloudfront.amazonaws.com"
-            },
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::${local.s3_bucket_name}/*",
-            "Condition": {
-                "StringEquals": {
-                    "AWS:SourceArn": "${aws_cloudfront_distribution.distribution.arn}"
-                }
-            }
-        }
-    ]
-}
-  EOF
-}
-
